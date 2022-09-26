41992621db4c3a52cf7785d8527965cf9ead2d0c
brahimdidi <brahimgb1team@gmail.com>
2022-09-26 16:29:55 +0000

    fix linters with -a

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
 
   has_many :expense_groups
-  has_many :expenses, :through => :expense_groups
+  has_many :expenses, through: :expense_groups
 
   validates :name, presence: true
 end
