afad3cb4c1a75d716cc80b796af5106ad5236fd6
brahimdidi <brahimgb1team@gmail.com>
2022-09-28 15:20:13 +0000

    edit routes if user is not authenticated set root to be home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :groups
   # Defines the root path route ("/")
-  root 'groups#index'
+
+  authenticated :user do
+    root 'groups#index', as: :root #-> if user is logged in
+    resources :controller #-> ONLY available for logged in users
+  end
+
+  unauthenticated :user do
+    root 'home#index', as: :unauthenticated #-> if user is not logged in
+  end
 end
