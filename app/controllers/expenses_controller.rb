28ceaf97a4a31dbce4e1ee5193cc4815a5299162
brahimdidi <brahimgb1team@gmail.com>
2022-09-29 20:45:39 +0000

    add expenses show page , fix n+1 queries

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,21 +1,24 @@
 class ExpensesController < ApplicationController
   def index
-    @expenses = Expense.all
+    @expenses = Expense.all.where(user: current_user).order('created_at DESC')
   end
 
   def show
     @user = User.find(current_user.id)
     @group = Group.find(params[:id])
-    @expensegroups = ExpenseGroup.where(group: @group)
+    @expensegroups = ExpenseGroup.where(group: @group).includes(expense: [:user]).order('created_at DESC')
+    @total_amount = 
     @expenses = []
     @expensegroups.each do |exgroup|
-      @expenses << exgroup.expense
+      if exgroup.expense.user == current_user
+        @expenses << exgroup.expense
+      end
     end
   end
 
   def new
     @user = current_user
-    @groups = Group.all
+    @groups = Group.all.where(user: current_user)
     expense = Expense.new
     respond_to do |format|
       format.html { render :new, locals: { expense: } }
