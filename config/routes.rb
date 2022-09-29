9ad6c9c7b4728314b6cf60c46fbdd5036d68ebfc
brahimdidi <brahimgb1team@gmail.com>
2022-09-29 16:33:17 +0000

    add route to create new group

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :groups
+  resources :users do 
+    resources :groups
+    resources :expenses
+  end
   # Defines the root path route ("/")
-
+  post 'users/:user_id/groups/new', :to => 'groups#create'
   authenticated :user do
     root 'groups#index', as: :root #-> if user is logged in
   end
