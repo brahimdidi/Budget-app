c0b80671eeb28d3347c06d76c7a8edfdafbb3f65
brahimdidi <brahimgb1team@gmail.com>
2022-09-29 21:32:54 +0000

    add test for expense model

---
+++ spec/models/expense_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Expense, type: :model do
+  before :each do
+    @user = User.new(name: 'test', email: 'test@gmail.com')
+    @expense = Expense.new(name: 'expense' , amount: 45, user: @user)
+  end
+
+  it 'shows exact name' do
+    expect(@expense.name).to eql ('expense')
+    expect(@expense.amount).to eql 45
+  end
+  it 'can not create a expense without  a name' do
+    expense = Expense.new(amount: 'expense amount', user: @user)
+    expect(expense.valid?).to eql false
+  end
+  it 'can not create a expense without  an amount' do
+    expense = Expense.new(name: 'expense ', user: @user)
+    expect(expense.valid?).to eql false
+  end
+end
