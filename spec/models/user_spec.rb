f41901e17d3d9be96b0e6407bbeb288c6665803c
brahimdidi <brahimgb1team@gmail.com>
2022-09-29 21:33:27 +0000

    add tests for groups model and users request

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before :each do
+    @user = User.new(name: 'test', email: 'test@gmail.com')
+  end
+  it 'shows exact name' do
+    expect(@user.name).to eql ('test')
+    expect(@user.email).to eql ('test@gmail.com')
+  end
+  it 'can not create a user without  a name' do
+    user = User.new(email: 'test@gmail.com')
+    expect(user.valid?).to eql false
+  end
+end
